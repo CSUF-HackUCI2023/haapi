8d7223c82e165e5b831a91a0a187ed6533ad9753
msk <15199219+muskit@users.noreply.github.com>
2023-02-04 15:42:34 -0800

    initial user-mode layout

---
+++ navigation/CustomerMode/CustomerContainer.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+import HomeScreen from './HomeScreen';
+import BookmarkScreen from './BookmarkScreen';
+
+const homeName = 'Home';
+const bookmarkName = 'Bookmarks';
+
+const Tab = createBottomTabNavigator();
+
+function CustomerContainer()
+{
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        switch (route.name)
+                        {
+                            case homeName:
+                                iconName = focused ? 'home' : 'home-outline';
+                                break;
+                            case bookmarkName:
+                                iconName = focused ? 'bookmarks' : 'bookmarks-outline';
+                        }
+
+                        return <Ionicons name={iconName} size={size} color={color}/>
+                    }
+                })}
+                >
+
+                <Tab.Screen name={homeName} component={HomeScreen} options={{headerShown: false}}/>
+                <Tab.Screen name={bookmarkName} component={BookmarkScreen} options={{headerShown: false}}/>
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default CustomerContainer;
---
+++ navigation/CustomerMode/SearchScreen.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+function HomeScreen()
+{
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
